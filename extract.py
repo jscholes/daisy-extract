e9b6f19b9fb42c4a1b5a9566a43eb2e4ed6a6fc6
James Scholes <james@jamesscholes.com>
2016-05-12 19:33:33 +0100

    Refactor DAISY book validation code to avoid repetition

--- extract.py
+++ extract.py
@@ -3,6 +3,7 @@
 # This program is free software, licensed under the terms of the GNU General Public License (version 3 or later).
 # See the file LICENSE for more details.
 
+from collections import namedtuple
 import argparse
 import glob
 import logging
@@ -25,17 +26,29 @@ NCC_FILENAME = 'NCC.HTML'
 MASTER_SMIL_FILENAME = 'MASTER.SMIL'
 SMIL_GLOB = '*.[sS][mM][iI][lL]'
 
+BookMetadata = namedtuple('BookMetadata', ('authors', 'title'))
+
+
+class DaisyExtractError(Exception):
+    pass
+
 
 def main():
     logger.info('daisy-extract version {0}'.format(__version__))
     cli_args = parse_command_line()
+    if cli_args.debug:
+        logger.setLevel(logging.DEBUG)
+
     input_directory = os.path.abspath(cli_args.input_directory)
     output_directory = os.path.abspath(cli_args.output_directory)
     if not os.path.exists(input_directory) or not os.path.isdir(input_directory):
         logger.error('{0} does not exist or is not a directory'.format(input_directory))
         sys.exit(1)
 
-    if not is_supported_daisy_book(input_directory):
+    try:
+        ncc_path = get_ncc_path(input_directory)
+        smil_files = get_smil_filenames(input_directory)
+    except DaisyExtractError as e:
         logger.error('The contents of {0} don\'t seem to be a valid DAISY 2.02 book.'.format(input_directory))
         sys.exit(1)
 
@@ -46,15 +59,32 @@ def parse_command_line():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-directory', nargs='?', required=True)
     parser.add_argument('-o', '--output-directory', nargs='?', required=True)
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Enable debug logging')
     args = parser.parse_args()
     return args
 
 
-def is_supported_daisy_book(path):
-    has_ncc = os.path.exists(os.path.join(path, NCC_FILENAME)) or os.path.exists(os.path.join(path, NCC_FILENAME.lower()))
-    smil_files = glob.iglob(os.path.join(path, SMIL_GLOB))
-    has_smils = len(list(filter(lambda smil: smil != MASTER_SMIL_FILENAME and smil != MASTER_SMIL_FILENAME.lower(), smil_files))) >= 1
-    return has_ncc and has_smils
+def get_ncc_path(directory):
+    filenames = (NCC_FILENAME, NCC_FILENAME.lower())
+    for filename in filenames:
+        path = os.path.join(directory, filename)
+        if os.path.exists(path) and os.path.isfile(path):
+            logger.debug('Found NCC file: {0}'.format(path))
+            return path
+
+    logger.debug('Could not find NCC file in directory')
+    raise DaisyExtractError('NCC file not found')
+
+
+def get_smil_filenames(directory):
+    smil_files = list(filter(lambda smil: smil != MASTER_SMIL_FILENAME and smil != MASTER_SMIL_FILENAME.lower(), glob.iglob(os.path.join(directory, SMIL_GLOB))))
+    number_of_smil_files = len(smil_files)
+    if number_of_smil_files >= 1:
+        logger.debug('Found {0} SMIL files in directory'.format(number_of_smil_files))
+        return smil_files
+    else:
+        logger.debug('No SMIL files found in directory')
+        raise DaisyExtractError('No SMIL files found')
 
 
 def make_safe_filename(filename):
