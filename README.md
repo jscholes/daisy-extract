3ef24cce6ea0c0e83a0d1ee73cdcef7c1ab5e6e7
James Scholes <james@jamesscholes.com>
2016-05-12 16:26:44 +0100

    Add basic script structure and update readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # daisy-extract
-Extract audio from DAISY talking books into a more audiobook-like structure
+This Python script will take a DAISY talking book, scan its metadata, and then extract all of the audio content into a friendlier structure.
+
+- By default the book will be placed in the directory: <output>\<author>\<title> (where <output> is the output path specified on the command line)
+- The MP3 files will be numbered and renamed to reflect their true contents
+- A playlist will be created to play the entire book
+
+Note: This script has only been tested with books which follow the DAISY 2.02 standard, designated as "Full audio with NCC only".
+
+## How do I use it?
+When I get around to it, I'll package the script so it will be installable with pip and runnable from anywhere.  But for now, to get set up:
+
+1. Install Python and pip.
+2. In a directory of your choice, run:
+    > git clone https://github.com/jscholes/daisy-extract
+    > cd daisy-extract
+    > pip install -r requirements.txt
+Alternatively you can download the contents of the repository as a ZIP file, extract it somewhere, and execute the two last commands above without cloning from Git.
+3. Run the script by typing:
+    python extract.py
+which will show you the possible command line options.
+
+Feel free to submit pull requests or file bugs here on GitHub.
--- extract.py
+++ extract.py
@@ -22,9 +22,35 @@ log_stream = logging.StreamHandler(sys.stdout)
 log_stream.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
 logger.addHandler(log_stream)
 
+NCC_FILENAME = 'NCC.HTML'
+
 
 def main():
     logger.info('daisy-extract version {0}'.format(__version__))
+    cli_args = parse_command_line()
+    input_directory = os.path.abspath(cli_args.input_directory)
+    output_directory = os.path.abspath(cli_args.output_directory)
+    if not os.path.exists(input_directory) or not os.path.isdir(input_directory):
+        logger.error('{0} does not exist or is not a directory'.format(input_directory))
+        sys.exit(1)
+
+    if not is_supported_daisy_book(input_directory):
+        logger.error('The contents of {0} don\'t seem to be a valid DAISY 2.02 book.'.format(input_directory))
+        sys.exit(1)
+
+    logger.info('Extracting content from {0} to {1}'.format(input_directory, output_directory))
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-directory', nargs='?', required=True)
+    parser.add_argument('-o', '--output-directory', nargs='?', required=True)
+    args = parser.parse_args()
+    return args
+
+
+def is_supported_daisy_book(path):
+    return os.path.exists(os.path.join(path, NCC_FILENAME)) or os.path.exists(os.path.join(path, NCC_FILENAME.lower()))
 
 
 if __name__ == '__main__':
